6186c844d305b47547d7be619350a6ae237012ad
conghungtran <tranconghung2023@gmail.com>
2025-11-23 17:47:39 +0700

    fix clang-format

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,15 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.snacks_animate = false
+-- Tự động format khi save
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = { "*.c", "*.h", "*.cpp", "*.hpp" },
+  callback = function()
+    vim.cmd(":!clang-format -i %")
+  end,
+})
+
+-- vim.diagnostic.enable(false, {
+--   virtual_text = false,
+--   signs = false,
+-- })
